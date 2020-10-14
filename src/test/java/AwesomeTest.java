6c42a9d3926a0b1912a859d86df7fe0843aff07d
merveinel <66885669+merveinel@users.noreply.github.com>
2020-10-14 07:58:54 +0100

    Added more awesome staff

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -4,4 +4,10 @@ public class AwesomeTest {
 
     //grey in.idea folder means it's ignored
     //when the color is normal, it means it's commited no changes
+
+    // I just pulled the new changes from github I did not have locally
+    // and now my project is in sync with remoye repository
+
+    //Now I made more awesome changes and I am about to push this back to github
+
 }
